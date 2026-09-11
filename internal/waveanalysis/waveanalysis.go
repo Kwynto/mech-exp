326a5c297acbcf997029f632503ac6093cc24c63
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2026-09-11 20:10:12 +0300

    Wave analysis, part 1

--- internal/waveanalysis/waveanalysis.go
+++ internal/waveanalysis/waveanalysis.go
@@ -19,7 +19,7 @@ const (
 	ADD_TO_BORER    = 1
 )
 
-var SlStGames []intypes.TStGame
+// var SlStGames []intypes.TStGame
 
 func initMapNumbers() intypes.TMapNembers {
 	initMap := make(intypes.TMapNembers, 40)
@@ -49,38 +49,17 @@ func initMapPoint() intypes.TMapPoints {
 	return initMap
 }
 
-func spaceSimbol(k int) string {
-	if (k / 10) >= 1 {
-		return ""
-	}
-	return " "
-}
+// func spaceSimbol(k int) string {
+// 	if (k / 10) >= 1 {
+// 		return ""
+// 	}
+// 	return " "
+// }
 
 func startAnalize(slStInput []intypes.TStGame, iGame int) {
-	var slWork, slWorkShort []intypes.TStGame
+	var slWork []intypes.TStGame
 
-	slStPrepear := slices.Clone(slStInput)
-	slices.Reverse(slStPrepear)
-
-	if iGame < 0 {
-		iGame = 0
-	}
-
-	iLenSlIn := len(slStPrepear)
-	if iLenSlIn < iGame {
-		iGame = iLenSlIn
-	}
-
-	if iGame == 0 {
-		fmt.Println(incolor.StringBlueH("Анализ всех тиражей."))
-		slWork = slStPrepear
-	} else {
-		sMsg1 := fmt.Sprintf("Анализ %d тиражей.", iGame)
-		fmt.Println(incolor.StringBlueH(sMsg1))
-		slWork = slStPrepear[0:iGame]
-	}
-
-	fmt.Println("")
+	slWork = slices.Clone(slStInput)
 
 	mStatNumbers := initMapNumbers()
 	mScorePoints := initMapPoint()
@@ -107,116 +86,39 @@ func startAnalize(slStInput []intypes.TStGame, iGame int) {
 			tempMStatNumber.Wrong = tempMStatNumber.Wrong + 1
 			mStatNumbers[iWrongNum] = tempMStatNumber
 		}
-	}
 
-	for iNumber, stStatNumber := range mStatNumbers {
-		tempScore := stStatNumber.PremiumWin * 3
-		// tempScore := stStatNumber.PremiumWin
-		tempScore = tempScore + stStatNumber.OrdinariWin
-		tempScore = tempScore - (stStatNumber.Wrong * 14)
-		mScorePoints[iNumber] = intypes.TPoint{
-			Number: iNumber,
-			Score:  tempScore,
-		}
-	}
-
-	// Short - begin
-	slWorkShort = slStPrepear[0:3]
-
-	mStatNumbersShort := initMapNumbers()
-
-	for _, stGame := range slWorkShort {
-		for i01, iWinNum := range stGame.Wins {
-			if i01 < PREMIUM_WIN {
-				tempMStatNumber := mStatNumbersShort[iWinNum]
-				tempMStatNumber.PremiumWin = tempMStatNumber.PremiumWin + 1
-				mStatNumbersShort[iWinNum] = tempMStatNumber
-			} else if (i01 >= PREMIUM_WIN) && (i01 < ORDINARI_WIN) {
-				tempMStatNumber := mStatNumbersShort[iWinNum]
-				tempMStatNumber.OrdinariWin = tempMStatNumber.OrdinariWin + 1
-				mStatNumbersShort[iWinNum] = tempMStatNumber
-			} else {
-				tempMStatNumber := mStatNumbersShort[iWinNum]
-				tempMStatNumber.Wrong = tempMStatNumber.Wrong + 1
-				mStatNumbersShort[iWinNum] = tempMStatNumber
+		// статистика за один раунд зафиксирована в mStatNumbers
+		// нужно посчитать баллы к этому раунду
+		for iNumber, stStatNumber := range mStatNumbers {
+			tempScore := stStatNumber.PremiumWin * 3
+			tempScore = tempScore + stStatNumber.OrdinariWin
+			tempScore = tempScore - (stStatNumber.Wrong * 14)
+			mScorePoints[iNumber] = intypes.TPoint{
+				Number: iNumber,
+				Score:  tempScore,
 			}
 		}
 
-		for _, iWrongNum := range stGame.Wrong {
-			tempMStatNumber := mStatNumbersShort[iWrongNum]
-			tempMStatNumber.Wrong = tempMStatNumber.Wrong + 1
-			mStatNumbersShort[iWrongNum] = tempMStatNumber
-		}
-	}
-
-	for iNumber, stStatNumber := range mStatNumbersShort {
-		tempScore := mScorePoints[iNumber].Score
-		tempScore = tempScore - (stStatNumber.Wrong * 4)
-		mScorePoints[iNumber] = intypes.TPoint{
-			Number: iNumber,
-			Score:  tempScore,
-		}
-	}
-	// Short - end
-
-	fmt.Println(incolor.StringBlue("Все номера:"))
-	for i := range NUMBERS_IN_GAME {
-		k1 := i + 1
-		for k, stScorePoint := range mScorePoints {
-			if k == k1 {
-				sMsg := ""
-
-				if stScorePoint.Score <= 0 {
-					sMsg = fmt.Sprintf("Номер %s%s = %d", spaceSimbol(stScorePoint.Number), incolor.StringRedH("%d", stScorePoint.Number), stScorePoint.Score)
-				} else if stScorePoint.Score >= 28 {
-					sMsg = fmt.Sprintf("Номер %s%s = %d", spaceSimbol(stScorePoint.Number), incolor.StringGreenH("%d", stScorePoint.Number), stScorePoint.Score)
-				} else {
-					sMsg = fmt.Sprintf("Номер %s%s = %d", spaceSimbol(stScorePoint.Number), incolor.StringBlackH("%d", stScorePoint.Number), stScorePoint.Score)
-				}
-
-				fmt.Println(sMsg)
+		// формируем срез отчета по баллам за выбранный раунд
+		if iGame == stGame.Game {
+			// fmt.Println(mScorePoints)
+			// fmt.Println(" ")
+			for i := 1; i < MAX_NUMBER+1; i++ {
+				tempNum := mScorePoints[i]
+				// fmt.Println(tempNum.Number, ": ", tempNum.Score)
+				fmt.Println(tempNum.Score)
 			}
 		}
-	}
 
-	fmt.Println("")
-	fmt.Println(incolor.StringGreen("Позитивный анализ:"))
-	for i := range NUMBERS_IN_GAME {
-		k1 := i + 1
-		for k, stScorePoint := range mScorePoints {
-			if k == k1 {
-				if stScorePoint.Score >= 28 {
-					sMsg := fmt.Sprintf("Номер %s%s = %d    %s  %s", spaceSimbol(stScorePoint.Number), incolor.StringGreenH("%d", stScorePoint.Number), stScorePoint.Score, incolor.StringBlackH("%s", "проигрыше было"), incolor.StringRedH("%d", mStatNumbers[k].Wrong))
-					fmt.Println(sMsg)
-				}
-			}
-		}
 	}
-
-	fmt.Println("")
-	fmt.Println(incolor.StringRed("Номера зоны риска:"))
-	for i := range NUMBERS_IN_GAME {
-		k1 := i + 1
-		for k, stScorePoint := range mScorePoints {
-			if k == k1 {
-				if stScorePoint.Score <= 0 {
-					sMsg := fmt.Sprintf("Номер %s%s = %d", spaceSimbol(stScorePoint.Number), incolor.StringRedH("%d", stScorePoint.Number), stScorePoint.Score)
-					fmt.Println(sMsg)
-				}
-			}
-		}
-	}
-
 }
 
 func Start(slStInput []intypes.TStGame) {
 	var sGame string
 
-	SlStGames = slStInput
+	fmt.Println(incolor.StringBlue("Координаты для графика волнового анализа:"))
 
-	fmt.Println(incolor.StringBlue("Балловый анализ тенденции результатов:"))
-
-	fmt.Print(incolor.StringMagenta("Кол-во последних тиражей для анализа (0 для всех тиражей) > "))
+	fmt.Print(incolor.StringMagenta("Номер раунда > "))
 	fmt.Scanf("%v\n", &sGame)
 	iGame, err1 := strconv.Atoi(sGame)
 	if err1 != nil {
